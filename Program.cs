f1238097b0df484468de243b6a7336f22344d44d
zohiMussawar <zohimussawar123@gmail.com>
2025-02-17 19:14:03 -0800

    second commit

--- Program.cs
+++ Program.cs
@@ -66,5 +66,11 @@ app.MapControllerRoute(
 
 app.MapRazorPages()
    .WithStaticAssets();
+ using (var scope = app.Services.CreateScope()) {
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ApplicationDbContext>();    
+    context.Database.Migrate();
+}
 
 app.Run();
